106701df65d86068a10e2a4a02bd21b8a4db1518
Fadi Haddad <fadi.haddad.235@gmail.com>
2023-03-06 10:58:48 +0200

    replaced fixed content with content from database for the new arrivals section

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+    const categories = document.getElementById("new-arrivals")
+  
+    axios.get('http://localhost/E-Commerce-Website-backend/index.php')
+      .then(function(response) {
+        const items = response.data;
+        console.log(items);
+  
+        items.forEach(item => {
+          const html = `
+            <div class="box">
+              <div class="image">
+                <a href="#"><img src="${item.src}" alt=""></a>
+              </div>
+              <div class="info">
+                <h3 class="title">${item.name}</h3>
+                <div class="subinfo">
+                  <div class="price">${item.price}</div>
+                </div>
+                <div class="add-remove-btn">
+                  <button class="add-button">add to cart</button>
+                  <button class="remove-button">add to favorite</button>
+                </div>
+              </div>
+            </div>
+          `;
+          categories.insertAdjacentHTML("beforeend", html);
+        });
+  
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
