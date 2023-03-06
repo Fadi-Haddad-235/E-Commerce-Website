7838e6c2338c976994ad06ec031dbffe4c6143af
MohammadAlAgha <mohalagha11@gmail.com>
2023-03-06 23:39:13 +0200

    Added cart items to the cart

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,3 +1,21 @@
 const prod = localStorage.getItem("cartItem");
 const parsed = JSON.parse(prod);
-console.log(parsed);
+const wrapper = document.getElementById("wrapper");
+let count = 1;
+
+parsed.forEach((item) => {
+  wrapper.innerHTML += `<div class="checkout">
+  <div class="prodImageName">
+    <div class="prodImage">
+      <img src="${item.item_img_scr}" alt="" />
+    </div>
+    <div class="prodName">${item.item_name}</div>
+  </div>
+  <div class="prodPrice">${item.item_price}$</div>
+  <div class="prodAddRemove">
+    <button class="Addremove" id="minus">-</button>
+    <p>${count}</p>
+    <button class="Addremove" id="plus">+</button>
+  </div>
+</div>`;
+});
